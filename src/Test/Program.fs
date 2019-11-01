1ef6733e449e4e18c7ae181d04a8ae79d277e675
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2019-11-01 21:47:21 +1000

    ✨ Add first attempt at property based testing

--- src/Test/Program.fs
+++ src/Test/Program.fs
@@ -27,10 +27,35 @@ let helperTests = testList "Helper Tests" [
     ]
 ]
 
+let invariants = testList "Properties and Invariants" [
+    testProperty "commands shouldn't result in invalid state or exception" <|
+        fun (commands: Parser.Command list) ->
+            let commands = commands |> List.filter (fun c -> c <> Quit)
+
+            let initialRobotState = [RobotState.zero ()]
+
+            let folder robotStates command =
+                match robotStates with
+                | [] -> robotStates
+                | head::_ -> REPL.commandHandler robotStates head command
+
+            let finalState = commands |> List.fold folder initialRobotState |> List.head
+            let bounds = finalState.Constraint
+
+            let x, y = finalState.Position
+
+            x < bounds.Width |> Expect.isTrue ""
+            x >= 0 |> Expect.isTrue ""
+
+            y < bounds.Height |> Expect.isTrue ""
+            y >= 0 |> Expect.isTrue ""
+]
+
 [<Tests>]
 let tests =
     testList "ToyRobot" [
         helperTests
+        invariants
     ]
 
 [<EntryPoint>]
