cca3daa57b59cc09646dc88a869925ef17056ef5
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2019-11-02 14:26:12 +1000

    🧹 Split out test modules into separate files

---
+++ src/Test/HelperTests.fs
@@ -0,0 +1,30 @@
+module Tests.Helpers
+
+open Expecto
+open Expecto.Flip
+
+open ToyRobot
+
+let helperTests = testList "Helper Tests" [
+    testList "Bounds Checker" [
+        testAsync "should report within bounds" {
+            let bounds : Bounds = { Height = 10; Width = 10;}
+            let validPosition : Position = 5, 5
+
+            Helpers.boundsCheck bounds validPosition
+            |> Expect.isSome "Position should be valid to place"
+        }
+
+        testAsync "should return `None` when out of bounds" {
+            let bounds : Bounds = { Height = 10; Width = 10;}
+            let invalidPositions : Position list = [
+                -1, 5; 10, 10
+                2, 11; 4, -1
+            ]
+
+            for position in invalidPositions do
+                Helpers.boundsCheck bounds position
+                |> Expect.isNone "Position should be invalid to place"
+        }
+    ]
+]
--- src/Test/Program.fs
+++ src/Test/Program.fs
@@ -1,61 +1,11 @@
 open Expecto
-open Expecto.Flip
 
-open ToyRobot
-
-let helperTests = testList "Helper Tests" [
-    testList "Bounds Checker" [
-        testAsync "should report within bounds" {
-            let bounds : Bounds = { Height = 10; Width = 10;}
-            let validPosition : Position = 5, 5
-
-            Helpers.boundsCheck bounds validPosition
-            |> Expect.isSome "Position should be valid to place"
-        }
-
-        testAsync "should return `None` when out of bounds" {
-            let bounds : Bounds = { Height = 10; Width = 10;}
-            let invalidPositions : Position list = [
-                -1, 5; 10, 10
-                2, 11; 4, -1
-            ]
-
-            for position in invalidPositions do
-                Helpers.boundsCheck bounds position
-                |> Expect.isNone "Position should be invalid to place"
-        }
-    ]
-]
-
-let invariants = testList "Properties and Invariants" [
-    testProperty "commands shouldn't result in invalid state or exception" <|
-        fun (commands: Parser.Command list) ->
-            let commands = commands |> List.filter (fun c -> c <> Quit)
-
-            let initialRobotState = [RobotState.zero ()]
-
-            let folder robotStates command =
-                match robotStates with
-                | [] -> robotStates
-                | head::_ -> REPL.commandHandler robotStates head command
-
-            let finalState = commands |> List.fold folder initialRobotState |> List.head
-            let bounds = finalState.Constraint
-
-            let x, y = finalState.Position
-
-            x < bounds.Width |> Expect.isTrue ""
-            x >= 0 |> Expect.isTrue ""
-
-            y < bounds.Height |> Expect.isTrue ""
-            y >= 0 |> Expect.isTrue ""
-]
-
+// Compose all tests into an annotated test list.
 [<Tests>]
 let tests =
     testList "ToyRobot" [
-        helperTests
-        invariants
+        Tests.Helpers.helperTests
+        Tests.Properties.invariants
     ]
 
 [<EntryPoint>]
@@ -67,6 +17,7 @@ let main argv =
         { defaultConfig with
             verbosity = Logging.LogLevel.Verbose
             colour = Logging.Colour256
+            fsCheckMaxTests = 100
         }
 
     runTestsWithArgs config argv tests
